87004440e0661f0339a6935410e8fd747f137a56
Jacek Partyka <j.partyka@weblogic.pl>
2019-01-03 01:26:01 +0100

    OTA with WiFi

---
+++ OTA-WiFi-boilerplate/OTA-WiFi-boilerplate.ino
@@ -0,0 +1,202 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#include <ESP8266WebServer.h>
+#include <FS.h>
+#include <ArduinoJson.h>
+
+ESP8266WebServer server;
+uint8_t pin_led = 2;
+char* ssid = ""; //not used
+char* password = "";
+char* mySsid = "ESP_OTA";
+
+IPAddress local_ip(192,168,11,4);
+IPAddress gateway(192,168,11,1);
+IPAddress netmask(255,255,255,0);
+
+bool ota_flag = false;
+uint16_t time_elapsed = 0;
+
+char webpage[] PROGMEM = R"=====(
+<html>
+<head>
+</head>
+<body>
+  <form>
+    <fieldset>
+      <div>
+        <label for="ssid">SSID</label>      
+        <input value="" id="ssid" placeholder="SSID">
+      </div>
+      <div>
+        <label for="password">PASSWORD</label>
+        <input type="password" value="" id="password" placeholder="PASSWORD">
+      </div>
+      <div>
+        <button class="primary" id="savebtn" type="button" onclick="myFunction()">SAVE</button>
+      </div>
+    </fieldset>
+  </form>
+</body>
+<script>
+function myFunction()
+{
+  console.log("button was clicked!");
+  var ssid = document.getElementById("ssid").value;
+  var password = document.getElementById("password").value;
+  var data = {ssid:ssid, password:password};
+  var xhr = new XMLHttpRequest();
+  var url = "/settings";
+  xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      // Typical action to be performed when the document is ready:
+      if(xhr.responseText != null){
+        console.log(xhr.responseText);
+      }
+    }
+  };
+  xhr.open("POST", url, true);
+  xhr.send(JSON.stringify(data));
+};
+</script>
+</html>
+)=====";
+
+void wifiConnect()
+{
+  //reset networking
+  WiFi.softAPdisconnect(true);
+  WiFi.disconnect();          
+  delay(1000);
+  //check for stored credentials
+  if(SPIFFS.exists("/config.json")){
+    const char * _ssid = "", *_pass = "";
+    File configFile = SPIFFS.open("/config.json", "r");
+    if(configFile){
+      size_t size = configFile.size();
+      std::unique_ptr<char[]> buf(new char[size]);
+      configFile.readBytes(buf.get(), size);
+      configFile.close();
+
+      DynamicJsonBuffer jsonBuffer;
+      JsonObject& jObject = jsonBuffer.parseObject(buf.get());
+      if(jObject.success())
+      {
+        _ssid = jObject["ssid"];
+        _pass = jObject["password"];
+        WiFi.mode(WIFI_STA);
+        WiFi.begin(_ssid, _pass);
+        unsigned long startTime = millis();
+        while (WiFi.status() != WL_CONNECTED) 
+        {
+          delay(500);
+          Serial.print(".");
+          digitalWrite(pin_led,!digitalRead(pin_led));
+          if ((unsigned long)(millis() - startTime) >= 5000) break;
+        }
+      }
+    }
+  }
+
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    digitalWrite(pin_led,HIGH);
+  } 
+  else 
+  {
+    WiFi.mode(WIFI_AP);
+    WiFi.softAPConfig(local_ip, gateway, netmask);
+    WiFi.softAP(mySsid); 
+    digitalWrite(pin_led,LOW);      
+  }
+  Serial.println("");
+  WiFi.printDiag(Serial);
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Booting");
+  SPIFFS.begin();
+  
+  WiFi.hostname(mySsid);
+
+  wifiConnect();
+
+  server.on("/",[](){server.send_P(200,"text/html", webpage);});
+  server.on("/toggle",toggleLED);
+  server.on("/settings", HTTP_POST, handleSettingsUpdate);
+  server.on("/update", handleOTAUpdate);
+  
+  ArduinoOTA.setPassword((const char *)"Jacek1");
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  pinMode(2,OUTPUT);
+  server.begin();
+}
+
+void handleSettingsUpdate()
+{
+  String data = server.arg("plain");
+  DynamicJsonBuffer jBuffer;
+  JsonObject& jObject = jBuffer.parseObject(data);
+
+  File configFile = SPIFFS.open("/config.json", "w");
+  jObject.printTo(configFile);  
+  configFile.close();
+  
+  server.send(200, "application/json", "{\"status\" : \"ok\"}");
+  delay(500);
+  
+  wifiConnect();
+}
+
+void handleOTAUpdate()
+{
+  ota_flag = true;
+  server.send(200, "text/plain", "You can upload the sketch for 30 seconds!");
+  time_elapsed = 0;
+}
+
+void loop() {
+  if (ota_flag)
+  {
+    uint16_t time_start = millis();
+    while(time_elapsed < 30000)
+    {
+      ArduinoOTA.handle();
+      time_elapsed = millis()-time_start;  
+      delay(10);
+    }
+    ota_flag = false;
+  }
+
+  server.handleClient();
+}
+
+void toggleLED()
+{
+  digitalWrite(pin_led,!digitalRead(pin_led));
+  server.send(204,"");
+}

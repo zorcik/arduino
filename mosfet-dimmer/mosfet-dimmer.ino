3820d5b4b9c94a3857f21265db06fe3af8191872
Jacek <j.partyka@weblogic.pl>
2024-10-30 17:02:56 +0100

    format

--- mosfet-dimmer/mosfet-dimmer.ino
+++ mosfet-dimmer/mosfet-dimmer.ino
@@ -8,7 +8,7 @@
 
 int address = 90;
 ModbusSerial mb(Serial, address, -1);
-uint8_t POSITIONS[] = {0, 1, 2, 3, 4, 5, 7, 9, 12, 15, 18, 22, 27, 32, 38, 44, 51, 58, 67, 76, 86, 96, 108, 120, 134, 148, 163, 180, 197, 216, 235, 255};
+uint8_t POSITIONS[] = {0, 1, 2, 3, 4, 5, 7, 9, 12, 15, 18, 22, 27, 32, 38, 44, 51, 58, 67, 76, 86, 96, 108, 120, 134, 148, 163, 180, 197, 216, 235, 255}; //32
 #define MAX 32
 
 int position = 0;
@@ -62,8 +62,6 @@ void loop()
         position = 0;
         knob.write(0);
     }
-
-
 
     if (ONOFF == LOW && !changed2)
     {

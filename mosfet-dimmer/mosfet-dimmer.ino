ece5a196182856a7ab03e61e25f083c51621d03a
Jacek <j.partyka@mserwis.pl>
2024-11-01 23:14:09 +0100

    fixed. final

--- mosfet-dimmer/mosfet-dimmer.ino
+++ mosfet-dimmer/mosfet-dimmer.ino
@@ -1,5 +1,6 @@
 #include <ModbusSerial.h>
 #include <Encoder.h>
+#include <Bounce2.h>
 
 #define ONOFF A1
 #define ENC1 2
@@ -9,12 +10,16 @@
 int address = 90;
 ModbusSerial mb(Serial, address, -1);
 uint8_t POSITIONS[] = {0, 1, 2, 3, 4, 5, 7, 9, 12, 15, 18, 22, 27, 32, 38, 44, 51, 58, 67, 76, 86, 96, 108, 120, 134, 148, 163, 180, 197, 216, 235, 255}; //32
-#define MAX 32
+#define MAX 128
 
 int position = 0;
+int arrayPosition = 0;
 bool status = 0;
+bool newStatus = 0;
+bool tempStatus = 1;
 bool changed = false;
 bool changed2 = false;
+Bounce onButton = Bounce();
 
 Encoder knob(ENC1, ENC2);
 
@@ -28,7 +33,10 @@ void setup()
 
     pinMode(ONOFF, INPUT_PULLUP);
     mb.addCoil(0, status);
-    mb.addIreg(0, position);
+    mb.addHreg(0, position);
+    mb.addHreg(1, arrayPosition);
+    onButton.attach(ONOFF, INPUT_PULLUP);
+    onButton.interval(25);
 }
 
 void loop()
@@ -41,46 +49,55 @@ void loop()
     if (newLeft != position)
     {
         position = newLeft;
-        changed = true;
+        if (position > MAX)
+        {
+            position = MAX;
+            knob.write(MAX);
+        }
+
+        if (position < 0)
+        {
+            position = 0;
+            knob.write(0);
+        }
+        mb.setHreg(0, position);
     }
 
-    int newPosition = mb.ireg(0);
-    if (newPosition != position && !changed)
+    int newPosition = mb.hreg(0);
+    if (newPosition != position)
     {
-        changed = true;
         position = newPosition;
-    }
-
-    if (position > MAX)
-    {
-        position = MAX;
-        knob.write(MAX);
-    }
-
-    if (position < 0)
-    {
-        position = 0;
-        knob.write(0);
-    }
-
-    if (ONOFF == LOW && !changed2)
-    {
-        delay(20);
-        if (ONOFF == LOW)
+        if (position > MAX)
         {
-            changed2 = true;
-            status != status;
+            position = MAX;
         }
+
+        if (position < 0)
+        {
+            position = 0;
+        }
+        knob.write(position);
     }
 
-    if (ONOFF == HIGH && changed2)
+    onButton.update();
+
+    if (onButton.changed())
     {
-        delay(20);
-        if (ONOFF == HIGH)
-        {
-            changed2 = false;
-        }
+        tempStatus = onButton.read();
+    }    
+
+    if (tempStatus == LOW)
+    {
+       status = !status;
+       mb.setCoil(0, status);
+       tempStatus = HIGH;
     }
+    
+    int newStatus = mb.coil(0);
+    if (newStatus != status)
+    {
+      status = newStatus;
+    } 
 
     if (!status)
     {
@@ -88,8 +105,16 @@ void loop()
     }
     else
     {
-        analogWrite(OUT, POSITIONS[position]);
+        arrayPosition = (int)position/4;
+        if (arrayPosition > 31)
+        {
+          arrayPosition = 31;
+        }
+        mb.setHreg(1, arrayPosition);
+        analogWrite(OUT, POSITIONS[arrayPosition]);
     }
 
-    mb.setIreg(0, position);
+    
+    
+    changed = false;
 }
